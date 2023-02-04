94a13b948ea0713883adade5871dd32ac5ddd5e5
Landon Murray <landonbmurray@icloud.com>
2023-02-04 15:39:50 -0500

    updated server js and connection to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
  const express = require('express')
+ const path = require('path')
  const bcrypt = require('bcrypt')
  const session = require('express-session');
  const exphbs = require('express-handlebars');
@@ -7,3 +8,14 @@
  
  const sequelize = require('./config/connection');
  const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+ const app = express();
+ const PORT = process.env.PORT || 3001;
+
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
