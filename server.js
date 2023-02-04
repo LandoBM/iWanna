b9c5e9ae84051a038cedddd6b82b7a498da09c73
Brian Yi <brianyi244@gmail.com>
2023-02-04 16:51:35 -0500

    db, utils, serverjs

--- server.js
+++ server.js
@@ -7,3 +7,14 @@
  
  const sequelize = require('./config/connection');
  const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+ const app = express();
+ const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
