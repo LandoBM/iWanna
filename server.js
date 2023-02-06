7895828171a6bac69586ce55c4b333ac2bdbe28f
Marcus Walker <mwalke35@yahoo.com>
2023-02-06 18:51:04 -0500

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
- const express = require('express')
- const bcrypt = require('bcrypt')
- const session = require('express-session');
- const exphbs = require('express-handlebars');
- const routes = require('./controllers');
- const helpers = require('./utils/helpers');
- 
- const sequelize = require('./config/connection');
- const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require('express')
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
+  
