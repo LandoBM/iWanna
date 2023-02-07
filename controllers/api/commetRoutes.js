f4175d9b0af6de1e7695aa64df07ec47ae4b67f8
Landon Murray <landonbmurray@icloud.com>
2023-02-07 14:11:31 -0500

    added route for comment

--- controllers/api/commetRoutes.js
+++ controllers/api/commetRoutes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment } = require('../../models/Comment');
+const withAuth = require('../../utils/auth')
+
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const commentInfo = await Comment.create(req.body)
+        
+        req.session.save(()=> {
+            req.session.user_id = userInfo.user_id
+        })
+
+    }catch (err){
+        res.status(404).json(err)
+    }
+})
