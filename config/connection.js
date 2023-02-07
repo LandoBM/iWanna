746a02e0ffe5b4b23328a6e0213895f0a1d910d6
Savannah Parris <savwillcha@gmail.com>
2023-02-06 21:08:26 -0500

    npm not working

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,8 @@
-const Sequelize =  require('sequelize')
 require('dotenv').config()
+const Sequelize =  require('sequelize')
+
+
+
+const sequelize = new Sequelize (process.env.MYSQL_URL)
+
+module.exports = sequelize
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS iwanna_db;
+CREATE DATABASE iwanna_db
+
