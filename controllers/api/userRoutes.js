503a50b1348232a750168f7c6ae45f41304253a6
Landon Murray <landonbmurray@icloud.com>
2023-02-06 20:55:47 -0500

    added routes for user, product, and comment

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req,res) => {
+    try{
+        const userInfo = await User.create(req.body)
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.id
+            req.session.logging_in = true;
+
+            res.status(200).json(userInfo)
+        })
+    } catch (err) {
+        res.status(404).json(err)
+    }
+})
