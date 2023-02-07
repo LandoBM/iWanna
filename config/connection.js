5a3a069d73de7a7c9499a69189372f896ba2edad
Savannah Parris <savwillcha@gmail.com>
2023-02-06 21:50:16 -0500

    push to branch

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,6 @@ require('dotenv').config()
 const Sequelize =  require('sequelize')
 
 
-<<<<<<< HEAD
-
-const sequelize = new Sequelize (process.env.MYSQL_URL)
-=======
 const sequelize = new Sequelize (process.env.MYSQL_URI)
->>>>>>> a8e2db441a8329f2421f1873d58f8d06999b4af7
 
 module.exports = sequelize
