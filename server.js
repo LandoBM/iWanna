12378276af6a5ad0bd66876da11f0d2a86c002fd
Marcus Walker <mwalke35@yahoo.com>
2023-02-06 21:30:41 -0500

    SENDING FILES

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
-const app = express();
-const PORT = process.env.PORT || 3003;
-
-
-
-
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-  });
-  
-=======
  const express = require('express')
  const path = require('path')
  const bcrypt = require('bcrypt')
@@ -32,7 +10,7 @@ app.listen(PORT, () => {
  const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
  const app = express();
- const PORT = process.env.PORT || 3001;
+ const PORT = process.env.PORT || 3003;
 
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
@@ -43,4 +21,3 @@ app.listen(PORT, () => {
  sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
->>>>>>> 1a787d4c3f9e0b394e5dbfd1d8e1d797c0ef7791
