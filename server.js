24d9dbe4fc161cbe12140256d60f86313f446e92
Audrey Alcantara <adrianaquin22@gmail.com>
2023-02-06 19:15:10 -0500

    connection set up

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const path = require('path')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,11 +12,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express()
 const PORT = process.env.PORT || 3001
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
