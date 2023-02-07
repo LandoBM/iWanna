c5bd9340fa0ac7e1473a8080a7c5e09773035322
Landon Murray <landonbmurray@icloud.com>
2023-02-07 14:07:11 -0500

    added routes for product

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -3,3 +3,37 @@ const { Product } = require('../../models/Product');
 const withAuth = require('../../utils/auth')
 
 
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const productInfo = await Product.create(req.body)
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.user_id
+        })
+        
+        res.status(200).json(productInfo)
+    }catch (err) {
+        res.status(404).json(err)
+    }
+})
+
+router.delete('/:id', withAuth, async (req,res) => {
+    try{
+        const productData = await Product.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        })
+
+        if(!productData) {
+            res.status(404).json({message: `There is no product related to this id!`})
+            return
+        }
+
+    } catch(err) {
+        res.status(404).json()
+    }
+})
+
+module.exports = router
