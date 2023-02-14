b2e6a6d908193e5a7411fcf9f720b42c7e87c1bd
Savannah Parris <savwillcha@gmail.com>
2023-02-13 20:16:27 -0500

    header footer homepage

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-=======
 //TESTING 
 const loginFormHandler = async(event) => {
     event.preventDefault()
@@ -79,4 +53,3 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
->>>>>>> 07aca660969a09b3328c4c50621f8b3217e020ab
