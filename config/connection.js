06ca06a9eb4ade24f18f1b5033356265548b8b51
Landon Murray <landonbmurray@icloud.com>
2023-02-14 19:16:32 -0500

    added dialect

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 const Sequelize =  require('sequelize')
 
 
-const sequelize = new Sequelize (process.env.MYSQL_URI)
+const sequelize = new Sequelize (
+    process.env.MYSQL_URI,
+    {
+        dialect:"mysql"
+    }
+)
 
 module.exports = sequelize
