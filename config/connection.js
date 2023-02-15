e7c0844e0fbc51568bff7f2a3169f630761d4ab2
Landon Murray <landonbmurray@icloud.com>
2023-02-14 19:39:19 -0500

    added process.env string

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
+const connString = process.env.JAWSDB_URI
 
-
-const sequelize = new Sequelize(process.env.JAWSDB_URI, {
+const sequelize = new Sequelize(connString, {
   dialect: "mysql",
 });
 
